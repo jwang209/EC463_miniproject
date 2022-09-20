d097a3889e7416bda942c51b10ac8e091f66c24d
jwang209 <65750291+jwang209@users.noreply.github.com>
2022-09-20 16:24:55 -0400

    Store just tweets not everything

--- tweet_pull.py
+++ tweet_pull.py
@@ -4,7 +4,8 @@ import json
 
 #Use this if you want to set terminal envrionment to save bearer token
 # bearer_token = os.environ.get("BEARER_TOKEN")
-bearer_token = 
+bearer_token = 'AAAAAAAAAAAAAAAAAAAAAHcWhAEAAAAAkLIhwxw5n86Z%2F9kXGd4j3U%2BYqQY%3DDqkOFob5P4RWa8Kgq8E2XiGyFx0u5oihSlCCBysoVxhKiWSZfz'
+
 search_url = "https://api.twitter.com/2/tweets/search/recent"
 
 # Optional params: start_time,end_time,since_id,until_id,max_results,next_token,
@@ -30,9 +31,13 @@ def connect_to_endpoint(url, params):
 
 def main():
     json_response = connect_to_endpoint(search_url, query_params)
-    # print(json.dumps(json_response, indent=0, sort_keys=True))
-    # print(type(json_response))
-    print(json_response["text"])
+
+    tweets = list()
+    for key in json_response:
+        for x in json_response[key]:
+            # print(type(x))
+            if isinstance(x, dict):
+                tweets.append(x['text'])
 
 if __name__ == "__main__":
     main()
